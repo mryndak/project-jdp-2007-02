e0b608770cc0957150b2007127aac7e4219725f6
matchris <krzysztof.matyjaszek@outlook.com>
2020-07-28 16:40:34 +0200

    Group Controller JDP200702-2

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/group")
 public class GroupController {
+    @RequestMapping
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{groupId}")
+    public GroupDto getGroup(@PathParam("groupId") Long groupId) {
+        return new GroupDto();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createGroup(@RequestBody GroupDto groupDto) {
+        return;
+    }
 }
+
