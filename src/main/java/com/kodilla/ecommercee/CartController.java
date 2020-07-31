b5575b978beaf6254889131fa4c9978f6361446e
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-07-31 14:24:20 +0200

    CartController endpoints implem., new classes at domain file: Cart, CartDto

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,42 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import com.kodilla.ecommercee.domain.ProductDto;
+import com.kodilla.ecommercee.exception.NotFoundException;
+import com.kodilla.ecommercee.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/ecommercee")
 public class CartController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getCarts")
+    public List<CartDto> getCarts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createCart")
+    public void createCart(@RequestBody CartDto cartDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getCart")
+    public CartDto getCart(@RequestParam Long id) throws NotFoundException {
+        return new CartDto();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteCart")
+    public void deleteCart(@RequestParam Long id) {
+    }
+
+//    @RequestMapping(method = RequestMethod.PUT, value = "addProductToCart")
+//    public void addProductToCart(long id, int quantity) {
+//    }
+//
+//    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProductFromCart")
+//    public void deleteProductFromCart(long id, int quantity) {
+//    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartDto {
+    private List<ProductDto> productList;
+    private BigDecimal totalPrice;
+    private int quantity;
+    private long userId;
+}
