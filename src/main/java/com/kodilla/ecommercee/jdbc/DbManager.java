9a7629a677a303b9809697f560fdbdff940664aa
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-08-02 18:06:32 +0200

    Delete @Repository annotation.

--- src/main/java/com/kodilla/ecommercee/jdbc/DbManager.java
+++ src/main/java/com/kodilla/ecommercee/jdbc/DbManager.java
@@ -1,14 +1,10 @@
 package com.kodilla.ecommercee.jdbc;
 
-
-import org.springframework.stereotype.Repository;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-@Repository
 public class DbManager {
     private Connection conn;
     private static DbManager dbManagerInstance;
