faea3f146e66d92eee2d4ff5ea196d130d7aa1fc
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-08-03 14:48:32 +0200

    Dodano id koszyka, skrocono statusy http zgodnie z commitami z 03.08

--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -1,9 +1,6 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.List;
