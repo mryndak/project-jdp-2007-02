c68262636a64bd28b7cb93540b12c458ff6ca6ac
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-08-06 18:53:58 +0200

    UserController implementation, Create UserMapper, Override 2 methods in Repository, implementation service.

---
+++ src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
@@ -0,0 +1,80 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.dto.UserDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserMapper {
+
+    public User mapToUser(final UserDto userDto) {
+        return new User(
+                userDto.getId(),
+                userDto.getUserName(),
+                userDto.getStatus(),
+                userDto.getUserKey(),
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getBirthDate(),
+                userDto.getRegisterDate(),
+                userDto.getEmail(),
+                userDto.getCity(),
+                userDto.getPostCode(),
+                userDto.getStreet(),
+                userDto.getHouseNr(),
+                userDto.getApartmentNr(),
+                userDto.getPhoneNumber(),
+                userDto.isBlocked(),
+                userDto.getLogin(),
+                userDto.getPassword());
+    }
+
+    public UserDto mapToUserDto(final User user) {
+        return new UserDto(
+                user.getId(),
+                user.getUserName(),
+                user.getStatus(),
+                user.getUserKey(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getBirthDate(),
+                user.getRegisterDate(),
+                user.getEmail(),
+                user.getCity(),
+                user.getPostCode(),
+                user.getStreet(),
+                user.getHouseNr(),
+                user.getApartmentNr(),
+                user.getPhoneNumber(),
+                user.isBlocked(),
+                user.getLogin(),
+                user.getPassword());
+    }
+
+    public List<UserDto> mapToUserDtoList(final List<User> usersList) {
+        return usersList.stream()
+                .map(user -> new UserDto(
+                        user.getId(),
+                        user.getUserName(),
+                        user.getStatus(),
+                        user.getUserKey(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getBirthDate(),
+                        user.getRegisterDate(),
+                        user.getEmail(),
+                        user.getCity(),
+                        user.getPostCode(),
+                        user.getStreet(),
+                        user.getHouseNr(),
+                        user.getApartmentNr(),
+                        user.getPhoneNumber(),
+                        user.isBlocked(),
+                        user.getLogin(),
+                        user.getPassword()))
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/kodilla/ecommercee/service/UserService.java
+++ src/main/java/com/kodilla/ecommercee/service/UserService.java
@@ -1,4 +1,35 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.exception.NotFoundException;
+import com.kodilla.ecommercee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository repository;
+
+    public List<User> getUsers() {
+        return repository.findAll();
+    }
+
+    public User getUser(final Long id) {
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    public void deleteUser(final Long id) {
+        repository.deleteById(id);
+    }
+
+    public User saveUser(final User user) {
+        return repository.save(user);
+    }
+
+    public void createUser(User user) {
+        repository.save(user);
+    }
 }
