34b855fcaf13ed371af2ca74420d1539ced3ee5d
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-08-06 17:59:23 +0200

    Usunięcie brancha z konfliktami, nowy branch JDP200702-14, implementacja encji Cart, repo CartDao

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -3,16 +3,72 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.implementation.auxiliary.AuxiliaryType;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "CARTS")
 public class Cart {
+
     private Long id;
-    //    private List<Product> productList;
     private BigDecimal totalPrice;
     private Integer quantity;
-    private Long userId;
+    // ZAKOMENTOWANE Z UWAGI NA BRAK KLAS (OBIEKTÓW) PRODUCT w drzewie master
+//    private Product product;
+//    private List<Product> products = new ArrayList<>();
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "CART_ID")
+    public Long getId() {
+        return id;
+    }
+
+    @NotNull
+    @Column(name = "TOTAL_PRICE")
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    @NotNull
+    @Column(name = "QUANTITY")
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+//    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
+//    public List<Product> getProductList() {
+//        return products;
+//    }
+
+    @NotNull
+    public User user;
+
+//@Id
+//@GeneratedValue
+//@NotNull
+//@Column(name = "CART_ID", unique = true)
+//    private Long id;
+//
+//    @NotNull
+//    @Column(name = "TOTAL_PRICE")
+//    private BigDecimal totalPrice;
+//
+//    @NotNull
+//    @Column(name = "QUANTITY")
+//    private Integer quantity;
+//
+//    @NotNull
+//    private User user;
 }
