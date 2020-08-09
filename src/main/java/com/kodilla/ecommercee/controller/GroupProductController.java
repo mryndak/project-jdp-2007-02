dc046617d1740a9e37a57ac407dff4cba04602d2
majafra <bartlomiej.wit@gmail.com>
2020-08-09 11:18:36 +0200

    two commmented corrupted User nad GroupController files

--- src/main/java/com/kodilla/ecommercee/controller/GroupProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupProductController.java
@@ -11,30 +11,30 @@ import java.util.List;
 @RequestMapping("v1/groups")
 public class GroupProductController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
-    public List<GroupDto> getGroups() {
-        GroupDto groupDto1 = new GroupDto(100L, "RTV");
-        GroupDto groupDto2 = new GroupDto(300L, "Bielizna");
-        GroupDto groupDto3 = new GroupDto(900L, "Obuwie");
-        List<GroupDto> groupDtoList = new ArrayList<>();
-        groupDtoList.add(groupDto1);
-        groupDtoList.add(groupDto2);
-        groupDtoList.add(groupDto3);
-        return groupDtoList;
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    public GroupDto getGroup(@PathParam("id") Long groupId) {
-        return new GroupDto(500L, "Małe AGD");
-    }
-
-    @RequestMapping(method = RequestMethod.PUT)
-    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
-        return new GroupDto(500L, "Some Updated Group");
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public void createGroup(@RequestBody GroupDto groupDto) {
-    }
+//    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+//    public List<GroupDto> getGroups() {
+//        GroupDto groupDto1 = new GroupDto(100L, "RTV");
+//        GroupDto groupDto2 = new GroupDto(300L, "Bielizna");
+//        GroupDto groupDto3 = new GroupDto(900L, "Obuwie");
+//        List<GroupDto> groupDtoList = new ArrayList<>();
+//        groupDtoList.add(groupDto1);
+//        groupDtoList.add(groupDto2);
+//        groupDtoList.add(groupDto3);
+//        return groupDtoList;
+//    }
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+//    public GroupDto getGroup(@PathParam("id") Long groupId) {
+//        return new GroupDto(500L, "Małe AGD");
+//    }
+//
+//    @RequestMapping(method = RequestMethod.PUT)
+//    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+//        return new GroupDto(500L, "Some Updated Group");
+//    }
+//
+//    @RequestMapping(method = RequestMethod.POST)
+//    public void createGroup(@RequestBody GroupDto groupDto) {
+//    }
 }
 
--- src/main/java/com/kodilla/ecommercee/controller/UserProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserProductController.java
@@ -10,58 +10,58 @@ import java.util.List;
 @RequestMapping("/v1/users")
 public class UserProductController {
 
-    @GetMapping
-    public List<UserDto> getUsers() {
-        return Arrays.asList(
-                UserDto.builder()
-                        .id(3L)
-                        .userName("Piotrek")
-                        .status(1)
-                        .userKey(59403L)
-                        .build(),
-                UserDto.builder()
-                        .id(2L)
-                        .userName("Admin")
-                        .status(1)
-                        .userKey(96997L)
-                        .build());
-    }
-
-    @GetMapping("/{id}")
-    public UserDto getUser(@PathVariable Long id) {
-        return new UserDto();
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-    }
-
-    @PutMapping("/{id}")
-    public UserDto updateUser(@PathVariable Long id,@RequestBody UserDto userDto) {
-        return new UserDto(
-                userDto.getId(),
-                userDto.getUserName(),
-                userDto.getStatus(),
-                userDto.getUserKey(),
-                userDto.getFirstName(),
-                userDto.getLastName(),
-                userDto.getBirthDate(),
-                userDto.getRegisterDate(),
-                userDto.getEmail(),
-                userDto.getCity(),
-                userDto.getPostCode(),
-                userDto.getStreet(),
-                userDto.getHouseNr(),
-                userDto.getApartmentNr(),
-                userDto.getPhoneNumber(),
-                userDto.isBlocked(),
-                userDto.getLogin(),
-                userDto.getPassword()
-        );
-    }
-
-    @PostMapping
-    public void createUser(@RequestBody UserDto userDto) {
-    }
+//    @GetMapping
+//    public List<UserDto> getUsers() {
+//        return Arrays.asList(
+//                UserDto.builder()
+//                        .id(3L)
+//                        .userName("Piotrek")
+//                        .status(1)
+//                        .userKey(59403L)
+//                        .build(),
+//                UserDto.builder()
+//                        .id(2L)
+//                        .userName("Admin")
+//                        .status(1)
+//                        .userKey(96997L)
+//                        .build());
+//    }
+//
+//    @GetMapping("/{id}")
+//    public UserDto getUser(@PathVariable Long id) {
+//        return new UserDto();
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteUser(@PathVariable Long id) {
+//    }
+//
+//    @PutMapping("/{id}")
+//    public UserDto updateUser(@PathVariable Long id,@RequestBody UserDto userDto) {
+//        return new UserDto(
+//                userDto.getId(),
+//                userDto.getUserName(),
+//                userDto.getStatus(),
+//                userDto.getUserKey(),
+//                userDto.getFirstName(),
+//                userDto.getLastName(),
+//                userDto.getBirthDate(),
+//                userDto.getRegisterDate(),
+//                userDto.getEmail(),
+//                userDto.getCity(),
+//                userDto.getPostCode(),
+//                userDto.getStreet(),
+//                userDto.getHouseNr(),
+//                userDto.getApartmentNr(),
+//                userDto.getPhoneNumber(),
+//                userDto.isBlocked(),
+//                userDto.getLogin(),
+//                userDto.getPassword()
+//        );
+//    }
+//
+//    @PostMapping
+//    public void createUser(@RequestBody UserDto userDto) {
+//    }
 }
 
