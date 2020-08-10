d8e7f06588af35a4db3b029ab36a46b497750ab0
matchris <krzysztof.matyjaszek@outlook.com>
2020-08-11 01:16:08 +0200

    some update by removing update functionality

---
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -0,0 +1,24 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class GroupMapper {
+    public Group mapToGroup(final GroupDto groupDto) {
+        return new Group(groupDto.getDescription());
+    }
+
+    public GroupDto mapToGroupDto(Group group) {
+        return new GroupDto(group.getId(),group.getName());
+    }
+
+    public List<GroupDto> mapToGroupDtoList(List<Group> groupList) {
+        return groupList.stream().map(group -> new GroupDto(group.getId(),group.getName()))
+                .collect(Collectors.toList());
+    }
+}
