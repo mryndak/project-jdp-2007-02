d9b0ea3619b6f9fb9373d5be978612f3953e36af
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-08-12 18:11:34 +0200

    Revert "Delete UserMapperBasicData and change fields orders and cart to DTO's in UserDto."
    This reverts commit 58c4b3e7

---
+++ src/main/java/com/kodilla/ecommercee/mapper/usermapper/UserMapperBasicData.java
@@ -0,0 +1,43 @@
+package com.kodilla.ecommercee.mapper.usermapper;
+
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.dto.UserDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserMapperBasicData {
+
+    public User mapToUser(final UserDto userDto) {
+        return User.builder()
+                .userName(userDto.getUserName())
+                .email(userDto.getEmail())
+                .registerDate(userDto.getRegisterDate())
+                .status(userDto.getStatus())
+                .isBlocked(userDto.isBlocked())
+                .build();
+    }
+
+    public UserDto mapToUserDto(final User user) {
+        return UserDto.builder()
+                .userName(user.getUserName())
+                .email(user.getEmail())
+                .registerDate(user.getRegisterDate())
+                .status(user.getStatus())
+                .isBlocked(user.isBlocked())
+                .build();
+    }
+
+    public List<UserDto> mapToUserDtoList(final List<User> usersList) {
+        return usersList.stream().map(u -> UserDto.builder()
+                .userName(u.getUserName())
+                .email(u.getEmail())
+                .registerDate(u.getRegisterDate())
+                .status(u.getStatus())
+                .isBlocked(u.isBlocked())
+                .build())
+                .collect(Collectors.toList());
+    }
+}
